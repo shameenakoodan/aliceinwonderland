0f6d1f0db1686785a99a6100b5f5d8a89ea8bc4e
ShameenaKoodan <shameena.koodan@gmail.com>
2022-09-08 15:51:02 -0500

    Added the the log information to the csv file

--- welcome.py
+++ welcome.py
@@ -1,4 +1,4 @@
-
+from csv import writer
 
 class AliceInWonderLand:
     
@@ -15,8 +15,12 @@ class AliceInWonderLand:
         while yesorno not in yes_no:
             yesorno = input("Do you want to enter the Wonder Land? Yes/No")
             if (yesorno).lower() == "yes":
+                log_data = ["Do you want to enter the Wonder Land?",yesorno]
+                self.log_info(log_data)
                 self.displayIntro()
             elif yesorno.lower() == "no":
+                log_data = ["Do you want to enter the Wonder Land?",yesorno]
+                self.log_info(log_data)
                 print("You missed meeting Alice the wonder girl")
                 quit()
             else:
@@ -62,8 +66,12 @@ class AliceInWonderLand:
         while graborfall not in grab_fall:
             graborfall = input("Would you like to grab something or let yourself fall? Grab/Fall")
             if graborfall.lower() == "grab":
+                log_data = ["Would you like to grab something or let yourself fall?",graborfall]
+                self.log_info(log_data)
                 self.grab()
             elif graborfall.lower() == "fall":
+                log_data = ["Would you like to grab something or let yourself fall?",graborfall]
+                self.log_info(log_data)
                 self.fall()
             else:
                 print("Enter grab/fall")
@@ -113,9 +121,13 @@ class AliceInWonderLand:
             thisorthat = input("Would you like to go this way or that way? This/That")
             print(thisorthat)
             if(thisorthat.lower() == "this"):
+                log_data = ["Would you like to go this way or that way?",thisorthat]
+                self.log_info(log_data)
                 self.this()
                 return
             elif(thisorthat.lower() == "that"):
+                log_data = ["Would you like to go this way or that way?",thisorthat]
+                self.log_info(log_data)
                 self.that()
             else:
                 print("Enter this/that")
@@ -132,7 +144,7 @@ class AliceInWonderLand:
         print("“Through my keyhole, into my garden!” says a")
         print("doorknob.")
         print("You jump with surprise. A talking doorknob!")
-        print("But you recover quickly and say, “How can I get")
+        print("But you recover quickly and say, “How can I get")    
         print("into your garden?”")
         print("“Just wish you were there!” says the doorknob. “I")
         print("find it’s much simpler that way.”")
@@ -152,10 +164,14 @@ class AliceInWonderLand:
         follow_explore=["follow","explore"]
         followorexplore = ''
         while followorexplore not in follow_explore:
-            followortexplore = input("Would you like to follow or explore? follow/explore")
+            followorexplore = input("Would you like to follow or explore? follow/explore")
             if(followorexplore.lower() == "follow"):
+                log_data = ["Would you like to follow or explore?",followorexplore]
+                self.log_info(log_data)
                 self.follow()
             elif(followorexplore.lower() == "explore"):
+                log_data = ["Would you like to follow or explore?",followorexplore]
+                self.log_info(log_data)
                 self.explore()
             else:
                 print("Enter follow/explore")
@@ -178,8 +194,12 @@ class AliceInWonderLand:
         while glovesorcookies not in gloves_cookies:
             glovesorcookies = input("Would you like to gloves or cookies? gloves/cookies")
             if(glovesorcookies.lower() == "gloves"):
+                log_data = ["Would you like to gloves or cookies?",glovesorcookies]
+                self.log_info(log_data)
                 self.gloves()
             elif(glovesorcookies.lower() == "cookies"):
+                log_data = ["Would you like to gloves or cookies?",glovesorcookies]
+                self.log_info(log_data)
                 self.cookies()
             else:
                 print("Enter gloves/cookies")
@@ -192,6 +212,7 @@ class AliceInWonderLand:
         print("You hope you won't be quite this big when you wake up from your dream!")
         print("******************** The End ********************")
         quit()
+
     def gloves(self):
         print("You look high and low for the White Rabbit’s gloves. In one room you find a closet with a heavy door. You go inside—and the door slams shut behind you. You try to open it but you can’t. You’re locked in!")
         print("“Help!” you ery, but you know that Alice can’t hear you through the thick door. You will have to open it yourself.")
@@ -210,8 +231,12 @@ class AliceInWonderLand:
         while somewhereorstay not in somewhere_stay:
             somewhereorstay = input("Do you want to go somewhere or stay and talk with the cat? somewhere/talk")
             if(somewhereorstay.lower() == "somewhere"):
+                log_data = ["Do you want to go somewhere or stay and talk with the cat?",somewhereorstay]
+                self.log_info(log_data)
                 self.somewhere()
             elif(somewhereorstay.lower() == "stay"):
+                log_data = ["Do you want to go somewhere or stay and talk with the cat?",somewhereorstay]
+                self.log_info(log_data)
                 self.stay()
             else:
                 print("Enter somewhere/stay")
@@ -221,6 +246,7 @@ class AliceInWonderLand:
         print("“Who’s Tweedledum?” asks Alice.")
         print("“Tt’s such a silly name,” you answer. “I’m sure it’s no one important.”")
         print("You want to follow a path that goes through the garden, but Alice isn’t sure that’s a good idea. The only other path, though, leads through some thick bushes.")
+
         
         #Give option to go to garden or bushes
         garden_bushes=["garden","bushes"]
@@ -228,12 +254,37 @@ class AliceInWonderLand:
         while gardenorbushes not in garden_bushes:
             gardenorbushes = input("Do you want to go through the garden or bushes? garden/bushes")
             if(gardenorbushes.lower() == "garden"):
+                log_data = ["Do you want to go through the garden or bushes?",gardenorbushes]
+                self.log_info(log_data)
                 self.garden()
             elif(gardenorbushes.lower() == "bushes"):
+                log_data = ["Do you want to go through the garden or bushes?",gardenorbushes]
+                self.log_info(log_data)
                 self.bushes()
             else:
                 print("Enter garden/bushes")
-    
+    def garden(self):
+        print("You step boldly into the garden, but Alice stands back.")
+        print("As soon as you enter, you feel a tug on your arm.")
+        print("“I’m Tweedledum,” says a roly-poly little man as he pulls you out of the garden. “Keep out!”")
+        print("Then you feel a tug on your other arm.")
+        print("“T’m Tweedledee, his brother,” says a look-alike man, as he pulls you into the garden. “Keep in!”")
+        print("You are wondering what’s going on when you see  the other side of the sign, the side that’s facing the garden. It says KEEP IN BY ORDER OF TWEEDLEDEE. Just then, Tweedledum and Tweedledee pull at the same time. You feel as if youre stretching.")
+        print("You look at your arms—you are!")
+        print("“Help!” you cry to Alice, but there’s nothing she can do.")
+        print("Oh, no! Now youre caught in a tug of war between two quarrelsome brothers, and you’re the rope!")
+        print("**************The End***************************")
+        quit()
+
+    def bushes(self):
+        print("When you come out on the other side of the thick bushes, you find yourselves in the middle of a tea party!")
+        print("A funny little man in a big hat shouts, “This is the March Hare’s un-birthday party. Did you bring a present?”")
+        print(" “Why, no,” you both say. “Good!” says the little man. “Then you’re un-invited!”")
+        print("“The Mad Hatter is right,” says the March Hare. “You're un-invited, so sit down!” You and Alice sit down. In a few minutes you are caught up in the craziest party you’ve ever been to.")
+        print("Everyone keeps switching places and switching cups, and the Hatter and Hare stuff a dormouse into the teapot! You’ve never had more fun at an un-birthday party in your life!")
+        print("**********The End**************")
+        quit()
+
     def explore(self):
         print("The White Rabbit scampers away, and you and Alice turn and run in the opposite direction. Soon you come to another garden.")
         print("“What kind of flowers are you?” asks a flower.")
@@ -297,8 +348,12 @@ class AliceInWonderLand:
         while somewhereortalk not in somewhere_talk:
             somewhereortalk = input("Would you like to go somwhere or talk? somewhere/talk")
             if(somewhereortalk == "somewhere"):
+                log_data = ["Would you like to go somwhere or talk?",somewhereortalk]
+                self.log_info(log_data)
                 self.somewhere()
             elif(somewhereortalk == "talk"):
+                log_data = ["Would you like to go somwhere or talk?",somewhereortalk]
+                self.log_info(log_data)
                 self.talk()
             else:
                 print("Enter somewhere/talk")
@@ -317,14 +372,36 @@ class AliceInWonderLand:
         garden_bushes=['garden','bushes']
         gardenorbushes = ''
         while gardenorbushes not in garden_bushes:
-            gardenorbushes= input("Would you like to go through garden or that way? garden/bushes")
+            gardenorbushes= input("Would you like to go through garden or bushes? garden/bushes")
             if(gardenorbushes == "garden"):
+                log_data = ["Would you like to go through garden or bushes?",gardenorbushes]
+                self.log_info(log_data)
                 self.garden()
             elif(gardenorbushes == "bushes"):
+                log_data = ["Would you like to go through garden or bushes?",gardenorbushes]
+                self.log_info(log_data)
                 self.bushes()
             else:
                 print("Enter garden/bushes")
+    def talk(self):
+        print("“What do you mean THAT WAY leads to THE END?” you ask the Cheshire Cat.")
+        print("But instead of answering, the cat starts to disappear!")
+        print("Soon all that is left is his grin. “Well,” says Alice, “I’ve often seen a cat without a grin, but a grin without a cat! It’s the most curious thing I ever saw in all my life!”")
+        print("“Me, too,” you answer. “And by the way, I’m more curious than ever to find out where THAT WAY leads!” So you and Alice continue down the path. In a little while you come to a grove of Weeping Willow trees.Suddenly Alice stubs her toe on a rock and starts to cry.")
+        print("You are trying to make her feel better, when a big drop of water falls on your head. Then another .. . and another and another. Is it raining? No! It’s the Weeping Willows. They    are crying with Alice! They cry so hard they start a small flood!")
+        print("“Stop crying, Alice,” you say. “You’re making the Weeping Willows cry, too!”")
+        print("“T can’t,” sobs Alice. “It hurts too much!”")
+        print("You try to help her up, but now the flood of tears ot has turned into a small river. You and Alice are swept along like two leaves.")
+        print("Suddenly you hear a roar. Oh, no! There’s a big waterfall ahead and you are heading right for it! “I hope this is all a dream,” you say. “Otherwise going THAT WAY really is going to lead to... ")
+        print("****************************  The End ****************************")
+        quit()
 
+    def log_info(self,data):
+        with open('log_info.csv', 'a', newline='') as f:
+            writer_object = writer(f)
+            # write the data
+            writer_object.writerow(data)
+            f.close()
 def main():
     name = input("Enter your name")
     aliceland = AliceInWonderLand(name)
